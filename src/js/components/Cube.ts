98957a201d9deee559cd4f43aec9e15b57673799
Patricia Steves <pats@hotmail.de>
2019-06-15 13:59:24 +0200

    Improve multiple rotations
    Open issue: Faces go bonkers, but at least the positions are correct now.

--- src/js/components/Cube.ts
+++ src/js/components/Cube.ts
@@ -1,4 +1,4 @@
-import { StandardMaterial, Scene, Vector3, TransformNode, ModelShape, Mesh, Animation } from '@babylonjs/core';
+import { StandardMaterial, Scene, Vector3, TransformNode, Mesh, Animation } from '@babylonjs/core';
 import { Block } from './Block';
 
 export class Cube {
@@ -7,6 +7,8 @@ export class Cube {
   material: StandardMaterial;
   scene: Scene;
   centerOffset: number = 0;
+  rotationCounter: number = 0;
+  gap: number = 0.05;
 
   constructor(size: number, scene: Scene) {
     this.size = size;
@@ -16,29 +18,43 @@ export class Cube {
     this.build();
   }
 
+  /**
+   * Build the internal cube reference
+   */
   build(): void {
-    this.model = new Array(this.size);
+    this.model = this._createMatrix(this.size, (matrix, x, y) => {
+      const key = '' + x + y;
 
-    for (let x = 0; x < this.size; x++) {
-      this.model[x] = new Array(this.size);
-
-      for (let y = 0; y < this.model[x].length; y++) {
-        const key = '' + x + y;
-        this.model[x][y] = new Array(this.size);
-
-        for (let z = 0; z < this.model[x][y].length; z++) {
-          this.model[x][y][z] = new Block(key + z, this.scene);
-        }
+      for (let z = 0; z < this.size; z++) {
+        matrix[x][y][z] = new Block(key + z, this.scene);
       }
-    }
+    });
   }
 
+  _createMatrix(size: number = this.size, fillFn?: (matrix, x, y) => void): Array<any> {
+    const matrix = new Array(size);
+
+    for (let x = 0; x < size; x++) {
+      matrix[x] = new Array(size);
+
+      for (let y = 0; y < size; y++) {
+        matrix[x][y] = new Array(size);
+
+        if (fillFn) fillFn(matrix, x, y);
+      }
+    }
+
+    return matrix;
+  }
+
+  /**
+   * Render and paint all individual blocks inside the cube.
+   */
   render(): void {
     for (let x = 0; x < this.model.length; x++) {
       for (let y = 0; y < this.model[x].length; y++) {
         for (let z = 0; z < this.model[x][y].length; z++) {
           const block = this.model[x][y][z];
-          const gap = 0.05;
           let faces = [];
 
           if (x === this.size - 1) {
@@ -61,20 +77,24 @@ export class Cube {
 
           block.setFaceColors(faces);
           block.render(new Vector3(
-            x - this.centerOffset - gap + (gap * x),
-            y - this.centerOffset - gap + (gap * y),
-            z - this.centerOffset - gap + (gap * z)
+            x - this.centerOffset - this.gap + (this.gap * x),
+            y - this.centerOffset - this.gap + (this.gap * y),
+            z - this.centerOffset - this.gap + (this.gap * z)
           ));
         }
       }
     }
   }
 
+  /**
+   * Animate the rotation of an axis by the specified amount.
+   */
   rotateAxis(axis: Vector3, amount: number) {
     const root = new TransformNode('rotationAxis');
     const axes = ['x', 'y', 'z'];
     let animationTarget: string = '';
     let boxArray: Array<Mesh> = [];
+    let modelClone: ([]|Block)[] = this._createMatrix();
 
     // get and group all boxes affected by the rotation
     for (let x = 0; x < this.model.length; x++) {
@@ -84,6 +104,7 @@ export class Cube {
               (axis.y > 0 && (y === axis.y - 1)) ||
               (axis.z > 0 && (z === axis.z - 1))) {
             boxArray.push(this.model[x][y][z].box);
+            modelClone[x][y][z] = this.model[x][y][z];
             this.model[x][y][z].box.parent = root;
           }
         }
@@ -107,15 +128,25 @@ export class Cube {
     keyframes.push({ frame: framerate / 4, value: amount });
     animation.setKeys(keyframes);
 
+
     this.scene.beginDirectAnimation(root, [animation], 0, framerate, false, undefined, () => {
       // once the animation is finished, apply the transformations
-      this._rotateInternal(boxArray, axis, amount);
+      this._rotateInternal(boxArray, modelClone, axis, amount);
     });
   }
 
-  _rotateInternal(boxes: Array<Mesh>, axis, amount) {
+  /**
+   * Update the rotation for all blocks inside the rotated axis
+   */
+  _rotateInternal(boxes: Array<Mesh>, modelClone: ([]|Block)[], axis, amount) {
     boxes.forEach(box => {
       const oldPos = box.position.clone();
+      const arrX = this._getIndexFromPosition(box.position.x);
+      const arrY = this._getIndexFromPosition(box.position.y);
+      const arrZ = this._getIndexFromPosition(box.position.z);
+      let newX = box.position.x;
+      let newY = box.position.y;
+      let newZ = box.position.z;
 
       // remove the box from the rotation animation parent
       box.parent = null;
@@ -125,15 +156,29 @@ export class Cube {
 
       // translate the representation to the updated position
       if (axis.x > 0) {
-        box.position.z = oldPos.y * Math.sign(amount);
-        box.position.y = oldPos.z * Math.sign(amount * -1);
+        newZ = oldPos.y * Math.sign(amount);
+        newY = oldPos.z * Math.sign(amount * -1);
       } else if (axis.y > 0) {
-        box.position.x = oldPos.z * Math.sign(amount);
-        box.position.z = oldPos.x * Math.sign(amount * -1);
+        newX = oldPos.z * Math.sign(amount);
+        newZ = oldPos.x * Math.sign(amount * -1);
       } else if (axis.z > 0) {
-        box.position.y = oldPos.x * Math.sign(amount);
-        box.position.x = oldPos.y * Math.sign(amount * -1);
+        newY = oldPos.x * Math.sign(amount);
+        newX = oldPos.y * Math.sign(amount * -1);
       }
+
+      box.position.x = newX;
+      box.position.y = newY;
+      box.position.z = newZ;
+
+      const newArrX = this._getIndexFromPosition(newX);
+      const newArrY = this._getIndexFromPosition(newY);
+      const newArrZ = this._getIndexFromPosition(newZ);
+      this.model[newArrX][newArrY][newArrZ] =
+           modelClone[arrX][arrY][arrZ];
     });
+  }
+
+  _getIndexFromPosition(position) {
+    return (position - (this.gap * Math.sign(position))) + 1;
   }
 }
