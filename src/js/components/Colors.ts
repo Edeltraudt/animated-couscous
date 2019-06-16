991bfa12277aeebb03b18daa294f922d637b278a
Patricia Steves <pats@hotmail.de>
2019-06-16 17:00:38 +0200

    Update colors

--- src/js/components/Colors.ts
+++ src/js/components/Colors.ts
@@ -1,11 +1,10 @@
 import { rgbToColor } from '../helpers';
 
 export const COLORS = [
-  { key: 'default', color: rgbToColor(30, 39, 46) },
-  { key: 'red', color: rgbToColor(234, 32, 39) },
-  { key: 'orange', color: rgbToColor(255, 121, 63) },
-  { key: 'yellow', color: rgbToColor(251, 197, 49) },
-  { key: 'green', color: rgbToColor(62, 165, 18) },
-  { key: 'blue', color: rgbToColor(0, 168, 255) },
-  { key: 'white', color: rgbToColor(245, 246, 250) }
+  { key: 'purple', color: rgbToColor(113, 34, 250) },
+  { key: 'orange', color: rgbToColor(255, 139, 139) },
+  { key: 'cyan', color: rgbToColor(71, 242, 255) },
+  { key: 'magenta', color: rgbToColor(254, 160, 254) },
+  { key: 'yellow', color: rgbToColor(253, 242, 0) },
+  { key: 'white', color: rgbToColor(250, 250, 255) }
 ];
